4f500adf7a566e6807221273f474f426534d6999
Juan Rodriguez <juan.j.rodriguez2@gmail.com>
2015-07-24 12:15:08 -0500

    adding loops and new array for specific colors

--- script.js
+++ script.js
@@ -9,22 +9,59 @@
 // $(this).data("clicks", !clicks);
 // });
 
-
+var newColorRed= []
 
 $("table").on("click", ".boxes", function() {
   console.log(event);
   $(this).css("background-color", "red");
   $("td").removeClass("boxes");
   $("td").addClass("boxes2");
-  console.log();
+  $(this).addClass("red");
+
+for (i= 0; i < color.length; i++) {
+	var colorRed = color[i].classList.contains("red");
+	if (colorRed === true) {
+		 newColorRed.push(color[i]);
+		
+	}
+	};
 });
 
 
 $("table").on("click", ".boxes2", function() {
 	$(this).css("background-color", "blue");
 	$("td").removeClass("boxes2");
-	$("td").addClass("boxes")
-    console.log("click");
+	$("td").addClass("boxes");
+	 $(this).addClass("blue");
+
+for (i= 0; i < color.length; i++) {
+	var colorBlue = color[i].classList.contains("blue");
+	if (colorBlue === true) {
+		 colorCountBlue();
+	}
+};
+    
 });
+
+
+
+function colorCountBlue() {
+	console.log("blue");
+}
+
+
+var tData = document.getElementsByTagName("td");
+var color = jQuery.makeArray( tData );
+
+
+
+
+
+
+
+
+
+
+
 
 
